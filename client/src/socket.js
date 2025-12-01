d927802ea6505efeaa9873efaee6b4460ae752b5
ganesh23101979 <ganesh.santhosh201@gmail.com>
2025-12-01 19:59:57 +0530

    interview-booking

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 export const socket = io(import.meta.env.VITE_SOCKET_URL, {
-    path: "/socket.io/",
+    path: import.meta.env.VITE_SOCKET_PATH,
     transports: ["websocket"],
 });
